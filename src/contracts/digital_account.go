dad55bb614bbdf23097337f721d5a59dc098c3ab
Vinícius Ferreira <vinidotnet@gmail.com>
2021-09-02 02:09:48 -0300

    wip: removing offer example and cleaning create pest usecase

--- src/contracts/digital_account.go
+++
@@ -1,6 +0,0 @@
-package contracts
-
-type InsertOffer func(offer string) error
-
-type DigitalAccountStore struct {
-}
--- src/main.go
+++ src/main.go
@@ -1,20 +1,7 @@
 package main
 
-import "pesthub/features/digitalaccount"
+import "fmt"
 
 func main() {
-
-	e := digitalaccount.NewCreateOffer(nil, nil)
-	e.Execute(nil)
-
-	// depsCreate := pest.CreateDeps{
-	// 	InsertCommand: database.Insert,
-	// }
-
-	// id, err := pest.Create(depsCreate, &pest.CreateInput{CommonName: "OurName"})
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println("Returned id: ", id)
+	fmt.Println("started")
 }
