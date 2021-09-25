2e2afd7363e87c3e059d877f0e32d95172c23e2b
Vinícius Ferreira <vinidotnet@gmail.com>
2021-09-25 10:46:50 -0300

    ref: api using env deps

--- src/adapters/api/api.go
+++ src/adapters/api/api.go
@@ -1,15 +1,12 @@
 package api
 
 import (
-	"pesthub/adapters/api/env"
 	"pesthub/adapters/api/handlers"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func NewApi(deps *env.ApiDependencies) *fiber.App {
-	setupDependencies(deps)
-
+func NewApi() *fiber.App {
 	config := fiber.Config{
 		AppName:      "Florestario",
 		ErrorHandler: ErrorHandler,
@@ -21,9 +18,4 @@ func NewApi(deps *env.ApiDependencies) *fiber.App {
 
 func setupHandlers(app *fiber.App) {
 	app.Post("/disorders", handlers.RegisterDisorderHandler)
-}
-
-func setupDependencies(deps *env.ApiDependencies) {
-	// todo: check all deps and panics if not exists
-	env.Deps = deps
 }
--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"pesthub/adapters/api"
-	"pesthub/adapters/api/env"
 	"pesthub/adapters/memdb"
 	"pesthub/adapters/testmsgs"
+	"pesthub/env"
 )
 
-func main() {
+func init() {
+	env.DisorderStore = memdb.NewMemoryDisorderStore()
+	env.MessageProvider = testmsgs.NewTestableMessageProvider()
+}
 
-	deps := &env.ApiDependencies{
-		DisorderStore:   memdb.NewMemoryDisorderStore(),
-		MessageProvider: testmsgs.NewTestableMessageProvider(),
-	}
-	app := api.NewApi(deps)
+func main() {
+	app := api.NewApi()
 	app.Listen(":8080")
 }
